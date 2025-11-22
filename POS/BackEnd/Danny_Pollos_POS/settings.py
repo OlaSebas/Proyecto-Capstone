88a7f8d7ba9700e9d4042bbcfcade783f82579db
OlaSebas <131685257+OlaSebas@users.noreply.github.com>
2025-11-22 01:37:37 -0300

    ETL detalle venta NO Run

--- POS/BackEnd/Danny_Pollos_POS/settings.py
+++ POS/BackEnd/Danny_Pollos_POS/settings.py
@@ -134,6 +134,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
+
 #CAMBIO DE URL PARA PRODUCCION
 #SITE_DOMAIN = "http://127.0.0.1:8000"
 SITE_DOMAIN = "https://dannypollosapp-ggbtgnbrapetc2b6.brazilsouth-01.azurewebsites.net"
